1f405dcd91062877a2b98f2d5e64b6d71ed35e78
trademail896-svg <trademail896@gmail.com>
2026-01-04 04:24:11 -0500

    Update app.py
    add flask app

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, request, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "Bot TradingView DEMO actif"
+
+@app.post("/webhook")
+def webhook():
+    data = request.get_json(silent=True) or {}
+    print("ALERTE REÇUE:", data)
+    return jsonify({"status": "ok"}), 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", "10000"))
+    app.run(host="0.0.0.0", port=port)
 
